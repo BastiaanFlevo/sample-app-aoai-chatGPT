17139cd1e2d63fb800b4ebaa77087416ea9b4e3a
BastiaanFlevo <bastiaan.vandentillaard@flevoland.nl>
2024-06-05 21:13:40 +0200

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -841,6 +841,30 @@ const Chat = () => {
                 </Stack>
               )}
               <Stack>
+                <CommandBarButton
+                    role="button"
+                    styles={{
+                      icon: {
+                        color: '#FFFFFF'
+                      },
+                      iconDisabled: {
+                        color: '#BDBDBD !important'
+                      },
+                      root: {
+                        color: '#FFFFFF',
+                        background:
+                          'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #000000 100%)'
+                      },
+                      rootDisabled: {
+                        background: '#F0F0F0'
+                      }
+                    }}
+                    className={styles.newChatIcon}
+                    iconProps={{ iconName: 'Add' }}
+                    onClick={newChat}
+                    disabled={disabledButton()}
+                    aria-label="start a new chat button"
+                  />
                 {appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured && (
                   <CommandBarButton
                     role="button"
